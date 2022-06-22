bde90ac9e710aba941befbd398bc8906e77f932e
shareysanr <shareysanr1@gmail.com>
2022-06-21 23:34:06 -0400

    Adding local storage

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromStorage)
+
+
+if (leadsFromStorage) {
+    myLeads = leadsFromStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     console.log(myLeads);
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
     renderLeads();
 })
 
